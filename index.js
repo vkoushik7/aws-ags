655bb999c720e8b8440ae608c683d5f5dd2c5fc6
krishna koushik <vkrishnakoushik7@gmail.com>
2024-06-28 11:27:37 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req,res) => {
+    res.send("hello, bugga");
+});
+
+app.get('/add',(req,res) => {
+    const num1 = parseInt(req.query.num1);
+    const num2 = parseInt(req.query.num2);
+    const sum = num1 + num2;
+    res.send("Sum is: " + sum);
+});
+
+app.listen(3000, () => {
+    console.log('server is running on port 3000');
+});
